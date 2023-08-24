1c074c6ea38adf1b0a44a12e9686e0650ee3ef80
Alex Rønne Petersen <alex@alexrp.com>
2023-08-24 19:58:09 +0200

    Remove all native memory manipulation APIs in Vezel.Novadrop.Client.
    Closes #85.

--- src/client/Memory/MemoryAccessor.cs
+++
@@ -1,8 +0,0 @@
-namespace Vezel.Novadrop.Memory;
-
-public abstract class MemoryAccessor
-{
-    public abstract void Read(NativeAddress address, scoped Span<byte> buffer);
-
-    public abstract void Write(NativeAddress address, scoped ReadOnlySpan<byte> buffer);
-}
