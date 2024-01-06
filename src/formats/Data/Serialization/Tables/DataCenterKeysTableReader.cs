vezel-dev/novadrop

commit a3233b9577b7ab703f5b39b0bc5aafc47d65efae

DataCenterKeysTableReader: Read the table eagerly.

src/formats/Data/Serialization/Tables/DataCenterKeysTableReader.cs
@@ -7,7 +7,7 @@ internal sealed class DataCenterKeysTableReader
 {
     private readonly DataCenterSimpleRegion<DataCenterRawKeys> _keys = new(false);
 
-    private readonly ConcurrentDictionary<int, DataCenterKeys> _cache = new();
+    private readonly List<DataCenterKeys> _byIndex = new(ushort.MaxValue);
 
     private readonly DataCenterStringTableReader _names;
 
@@ -22,39 +22,39 @@ internal sealed class DataCenterKeysTableReader
         await _keys.ReadAsync(reader, cancellationToken).ConfigureAwait(false);
     }
 
+    public void Populate()
+    {
+        // This has to happen after the names string table has been read.
+
+        for (var i = 0; i < _keys.Elements.Count; i++)
+        {
+            string? GetName(int index)
+            {
+                var nameIdx = index - 1;
+
+                if (nameIdx == -1)
+                    return null;
+
+                var name = _names.GetString(nameIdx);
+
+                Check.Data(
+                    name != DataCenterConstants.ValueAttributeName,
+                    $"Key entry refers to illegal attribute name '{name}'.");
+
+                return name;
+            }
+
+            var raw = _keys.Elements[i];
+
+            _byIndex[i] = new(
+                GetName(raw.NameIndex1), GetName(raw.NameIndex2), GetName(raw.NameIndex3), GetName(raw.NameIndex4));
+        }
+    }
+
     public DataCenterKeys GetKeys(int index)
     {
-        Check.Data(
-            index < _keys.Elements.Count, $"Keys table index {index} is out of bounds (0..{_keys.Elements.Count}).");
+        Check.Data(index < _byIndex.Count, $"Keys table index {index} is out of bounds (0..{_byIndex.Count}).");
 
-        return _cache.GetOrAdd(
-            index,
-            static (i, @this) =>
-            {
-                string? GetName(int index)
-                {
-                    var nameIdx = index - 1;
-
-                    if (nameIdx == -1)
-                        return null;
-
-                    var name = @this._names.GetString(nameIdx);
-
-                    Check.Data(
-                        name != DataCenterConstants.ValueAttributeName,
-                        $"Key entry refers to illegal attribute name '{name}'.");
-
-                    return name;
-                }
-
-                var raw = @this._keys.Elements[i];
-
-                return new(
-                    GetName(raw.NameIndex1),
-                    GetName(raw.NameIndex2),
-                    GetName(raw.NameIndex3),
-                    GetName(raw.NameIndex4));
-            },
-            this);
+        return _byIndex[index];
     }
 }

src/formats/Data/Serialization/Readers/DataCenterReader.cs
@@ -223,6 +223,9 @@ internal abstract class DataCenterReader
                         await _nodes.ReadAsync(strict, reader, cancellationToken).ConfigureAwait(false);
                         await _values.ReadAsync(strict, reader, cancellationToken).ConfigureAwait(false);
                         await _names.ReadAsync(strict, reader, cancellationToken).ConfigureAwait(false);
+
+                        _keys.Populate();
+
                         await _footer.ReadAsync(strict, reader, cancellationToken).ConfigureAwait(false);
 
                         Check.Data(

src/formats/Data/Serialization/Tables/DataCenterKeysTableWriter.cs
@@ -7,7 +7,7 @@ internal sealed class DataCenterKeysTableWriter
 {
     private readonly DataCenterSimpleRegion<DataCenterRawKeys> _keys = new(false);
 
-    private readonly Dictionary<(string?, string?, string?, string?), int> _cache = new(ushort.MaxValue);
+    private readonly Dictionary<(string?, string?, string?, string?), int> _indexes = new(ushort.MaxValue);
 
     private readonly DataCenterStringTableWriter _names;
 
@@ -26,7 +26,7 @@ internal sealed class DataCenterKeysTableWriter
     {
         var tup = (attributeName1, attributeName2, attributeName3, attributeName4);
 
-        ref var index = ref CollectionsMarshal.GetValueRefOrAddDefault(_cache, tup, out var exists);
+        ref var index = ref CollectionsMarshal.GetValueRefOrAddDefault(_indexes, tup, out var exists);
 
         if (!exists)
         {

src/formats/Data/Serialization/Tables/DataCenterStringTableReader.cs
@@ -11,9 +11,9 @@ internal sealed class DataCenterStringTableReader
 
     private readonly DataCenterSimpleRegion<DataCenterRawAddress> _addresses = new(true);
 
-    private readonly Dictionary<DataCenterAddress, string> _addressCache = new(ushort.MaxValue);
+    private readonly Dictionary<DataCenterAddress, string> _byAddress = new(ushort.MaxValue);
 
-    private readonly List<string> _indexCache = new(ushort.MaxValue);
+    private readonly List<string> _byIndex = new(ushort.MaxValue);
 
     public DataCenterStringTableReader(int count)
     {
@@ -86,26 +86,25 @@ internal sealed class DataCenterStringTableReader
                     Check.Data(i == bucket, $"String bucket {i} does not match expected bucket {bucket}.");
                 }
 
-                Check.Data(_addressCache.TryAdd(addr, value), $"String address {addr} already recorded earlier.");
+                Check.Data(_byAddress.TryAdd(addr, value), $"String address {addr} already recorded earlier.");
 
                 cache.Add((index, value));
             }
         }
 
-        foreach (var (_, val) in cache.OrderBy(static tup => tup.Index))
-            _indexCache.Add(val);
+        _byIndex.AddRange(cache.OrderBy(static tup => tup.Index).Select(static tup => tup.Value));
     }
 
     public string GetString(int index)
     {
-        Check.Data(index < _indexCache.Count, $"String table index {index} is invalid.");
+        Check.Data(index < _byIndex.Count, $"String table index {index} is out of bounds (0..{_byIndex.Count}).");
 
-        return _indexCache[index];
+        return _byIndex[index];
     }
 
     public string GetString(DataCenterAddress address)
     {
-        Check.Data(_addressCache.TryGetValue(address, out var str), $"String table address {address} is invalid.");
+        Check.Data(_byAddress.TryGetValue(address, out var str), $"String table address {address} is invalid.");
 
         return str;
     }

src/formats/Data/Serialization/Tables/DataCenterStringTableWriter.cs
@@ -11,7 +11,7 @@ internal sealed class DataCenterStringTableWriter
 
     private readonly DataCenterSimpleRegion<DataCenterRawAddress> _addresses = new(true);
 
-    private readonly Dictionary<string, DataCenterRawString> _cache = new(ushort.MaxValue);
+    private readonly Dictionary<string, DataCenterRawString> _entries = new(ushort.MaxValue);
 
     private readonly bool _limit;
 
@@ -35,7 +35,7 @@ internal sealed class DataCenterStringTableWriter
 
     public DataCenterRawString AddString(string value)
     {
-        ref var raw = ref CollectionsMarshal.GetValueRefOrAddDefault(_cache, value, out var exists);
+        ref var raw = ref CollectionsMarshal.GetValueRefOrAddDefault(_entries, value, out var exists);
 
         if (!exists)
         {
@@ -108,6 +108,6 @@ internal sealed class DataCenterStringTableWriter
 
     public DataCenterRawString GetString(string value)
     {
-        return _cache[value];
+        return _entries[value];
     }
 }

commit 74483ee9e4cfa1cb6c836283789c42db588cae7f

Fix a data center deserialization regression in a3233b9.

src/formats/Data/Serialization/Tables/DataCenterKeysTableReader.cs
@@ -26,7 +26,7 @@ internal sealed class DataCenterKeysTableReader
     {
         // This has to happen after the names string table has been read.
 
-        for (var i = 0; i < _keys.Elements.Count; i++)
+        foreach (var raw in _keys.Elements)
         {
             string? GetName(int index)
             {
@@ -44,10 +44,8 @@ internal sealed class DataCenterKeysTableReader
                 return name;
             }
 
-            var raw = _keys.Elements[i];
-
-            _byIndex[i] = new(
-                GetName(raw.NameIndex1), GetName(raw.NameIndex2), GetName(raw.NameIndex3), GetName(raw.NameIndex4));
+            _byIndex.Add(new(
+                GetName(raw.NameIndex1), GetName(raw.NameIndex2), GetName(raw.NameIndex3), GetName(raw.NameIndex4)));
         }
     }
 